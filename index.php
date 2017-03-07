41c482378a308f218b22f4b3de96e82c8d265644
Kenneth Laqui <kennethlaqui@yahoo.com>
2017-03-07 15:42:38 +0800

    add link for browhaus facebook page

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
 								 
 							</span> 
 							<span class="li-social">
-								<a href="#"><i class="fa fa-facebook"></i></a> 
+								<a href="https://www.facebook.com/BrowhausManila/" target="_blank"><i class="fa fa-facebook"></i></a> 
 								<a href="#"><i class="fa fa-twitter"></i></a> 
 								<a href="#"><i class="fa fa-envelope"></i></a> 
 								<a href="#"><i class="fa fa-skype"></i></a>
